d11939bd5e3837d37c25955173794c5b6457a514
Paulo matos <paulo.dematos23@gmail.com>
2026-08-18 20:51:25 -0300

    conexão do db e criação dos primeiros modelos e dados

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,49 @@
-import os
-from flask import Flask
+import click
+import sqlalchemy as sa
+from datetime import datetime
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<User {self.username}>"
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    content: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r} "
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+        SQLALCHEMY_DATABASE_URI='sqlite:///db.sqlite',
     )
 
     if test_config is None:
@@ -14,10 +51,9 @@ def create_app(test_config=None):
     else:
         
         app.config.from_mapping(test_config)
-        
-    from . import db
-    
+
+
+    app.cli.add_command(init_db_command)        
     db.init_app(app)
-    # ensure the instance folder exists
-    os.makedirs(app.instance_path, exist_ok=True)
+    
     return app
