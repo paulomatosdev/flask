0be41507862046de5720091bbaa0a882472de0e5
Paulo matos <paulo.dematos23@gmail.com>
2026-08-18 18:02:28 -0300

    configurações adicionais

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,23 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+    )
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        
+        app.config.from_mapping(test_config)
+        
+    from . import db
+    
+    db.init_app(app)
+    # ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
+    return app
