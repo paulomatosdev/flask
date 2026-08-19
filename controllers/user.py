b41026c9b0c231be89a35763d47f28def1ce114f
Paulo matos <paulo.dematos23@gmail.com>
2026-08-18 22:41:03 -0300

    remove arquivos e estrutura desnecessária; adiciona implementação inicial do banco de dados e modelos

---
+++ controllers/user.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request
+from src.app import User, db
+from http import HTTPStatus
+
+app = Blueprint("user", __name__, url_prefix="/users")
+
+def _create_user():
+    data = request.json
+    user = User(username=data["username"])
+    db.session.add(user)
+    db.session.commit()
+
+def _list_users():
+    query = db.select(User)
+    results = db.session.execute
+    return results
+
+
+@app.route("/", methods=["GET", "POST"])
+def handle_user():
+    if request.method == "POST":
+        _create_user()
+        return {"message": "User created"}, HTTPStatus.CREATED
+    else :
+        return {"message": "Users"}
+    
--- src/app.py
+++ src/app.py
@@ -55,5 +55,8 @@ def create_app(test_config=None):
 
     app.cli.add_command(init_db_command)        
     db.init_app(app)
-    
+
+    from controllers import user
+    app.register_blueprint(user.app)
+
     return app
