cdf6144ebd1c3fb9e3915007a65b9b7f263da5d3
Paulo matos <paulo.dematos23@gmail.com>
2026-08-18 23:55:20 -0300

    adiciona rotas para obter, atualizar e deletar usuários; refatora função de listagem

--- controllers/user.py
+++ controllers/user.py
@@ -1,26 +1,66 @@
 from flask import Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
+from sqlalchemy import inspect
 
 app = Blueprint("user", __name__, url_prefix="/users")
+
 
 def _create_user():
     data = request.json
     user = User(username=data["username"])
     db.session.add(user)
     db.session.commit()
+    return user
+
 
 def _list_users():
     query = db.select(User)
     users = db.session.execute(query).scalars()
     return [
         {
-        "id": user.id, 
-        "username": user.username,
-        } 
+            "id": user.id,
+            "username": user.username,
+        }
         for user in users
     ]
-    
+
+
+@app.route('/<int:user_id>')
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return {
+        "id": user.id,
+        "username": user.username,
+    }
+
+
+@app.route('/<int:user_id>', methods=["PATCH"])
+def update_user(user_id):
+    user = db.get_or_404(User, user_id)
+    data = request.json
+
+    mapper = inspect(User)
+    assert mapper is not None  # garante ao type checker que inspect() retornou um mapper válido
+
+    for column in mapper.attrs:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
+    db.session.commit()
+
+    return {
+        "id": user.id,
+        "username": user.username,
+    }
+
+
+@app.route('/<int:user_id>', methods=["DELETE"])
+def delete_user(user_id):  # renomeado de get_user -> delete_user (era o nome duplicado)
+    user = db.get_or_404(User, user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return "", HTTPStatus.NO_CONTENT
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -28,14 +68,5 @@ def handle_user():
     if request.method == "POST":
         _create_user()
         return {"message": "User created"}, HTTPStatus.CREATED
-    else :
+    else:
         return {"message": "Users"}
-
-@app.route('/<int:user_id>')
-def get_uder(user_id):
-    user = db.get_or_404(User, user_id)
-    return{
-        "id": user.id, 
-                "username": user.username,
-    }
-    
