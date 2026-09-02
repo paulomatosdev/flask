a5ca502b538bdae14d30de1130e9087cdd52375b
Paulo matos <paulo.dematos23@gmail.com>
2026-09-01 21:37:49 -0300

    refatora testes para a função de elevação ao quadrado, utilizando parametrização para melhor cobertura de casos

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
+import pytest
 from src.utils import eleva_quadrado
 
-def test_eleva_quadrado_sucesso():
-    resultado = eleva_quadrado(2)
-    assert resultado == 4
+@pytest.mark.parametrize("test_input,expected", [(2, 4), (10, 100), (3, 9)])
+def test_eleva_quadrado_sucesso(test_input, expected):
+    resultado = eleva_quadrado(test_input)
+    assert resultado == expected
     
